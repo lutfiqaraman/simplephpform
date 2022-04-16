0f2b6f46838ef0a2df88c2817f35a0ced8f9bfe0
lutfiqaraman <lutfiqaraman@gmail.com>
2022-04-16 21:20:00 +0300

    build a register form

--- form.php
+++ form.php
@@ -1,5 +1,53 @@
 <?php
-    
+
+    $username  = '';
+    $password  = '';
+    $gender    = '';
+    $color     = '';
+    $languages = [];
+    $comments  = '';
+    $tc        = '';
+
+    if (isset($_POST['submit'])) {
+
+        if(isset($_POST['username'])) {
+            $username = $_POST['username'];
+        }
+
+        if(isset($_POST['password'])) {
+            $password = $_POST['password'];
+        }
+
+        if(isset($_POST['gender'])) {
+            $gender = $_POST['gender'];
+        }
+
+        if(isset($_POST['color'])) {
+            $color = $_POST['color'];
+        }
+
+        if(isset($_POST['languages'])) {
+            $languages = $_POST['languages'];
+        }
+
+        if(isset($_POST['comments'])) {
+            $comments = $_POST['comments'];
+        }
+
+        if(isset($_POST['tc'])) {
+            $tc = $_POST['tc'];
+        }
+
+        printf('User name: %s<br>Password: %s<br>Gender: %s<br>Color: %s<br>Language(s): %s<br>Comments: %s<br>T&smp;C: %s',
+            htmlspecialchars($username, ENT_QUOTES),
+            htmlspecialchars($password, ENT_QUOTES),
+            htmlspecialchars($gender, ENT_QUOTES),
+            htmlspecialchars($color, ENT_QUOTES),
+            htmlspecialchars(implode('', $languages), ENT_QUOTES),
+            htmlspecialchars($comments, ENT_QUOTES),
+            htmlspecialchars($tc, ENT_QUOTES)
+        );
+    }
 ?>
 
 <form action="" method="post">
@@ -32,6 +80,6 @@
     <input type="checkbox" name="tc" value="ok">
     I accept the T&amp;C
     <br>
-    
-    <input type="submit" value="Register">
+
+    <input type="submit" name="submit" value="Register">
 </form>
